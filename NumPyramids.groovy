1c1aea34663923c34cec67a63185df36f381e29f
RightyHo <andrewho@Andrews-iMac.local>
2013-10-09 13:30:33 +0100

    Added a couple of loops to automate the print pyramid

--- NumPyramids.groovy
+++ NumPyramids.groovy
@@ -1,6 +1,16 @@
 print "How many layers do you want on your pyramid? (1-9):"
-int num = Integer.parseInt(System.console().readLine())
-int count = 0
+int numFloors = Integer.parseInt(System.console().readLine())
+int level = 1
 
-while(count < num){
-		
+while(level <= numFloors){
+	int count = 0
+	while(count < level){
+		print level
+		count++
+	}
+	println " "
+	level++
+}
+
+	
+	
