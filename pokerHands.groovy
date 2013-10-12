0aab3b11cf27dab28f2cc2df220f2810db238a9a
RightyHo <andrewho@Andrews-iMac.local>
2013-10-12 19:11:03 +0100

    complete rehaul of code adding methods for different tasks

--- pokerHands.groovy
+++ pokerHands.groovy
@@ -1,34 +1,59 @@
-boolean validCard = true
-
-print "Please key in the rank and suit of your first card (eg; 5H for the Five of Hearts):"
-String card1 = System.console().readLine()
-
-//check if the rank is 10 which is only situation when string should be longer than 2 chars
-
-if(card1.length()>2){
-	String rank1 = card1.substring(0,2)
-	if(rank1 != "10"){
-		validCard = false
-		println card1 + "is not a valid card"
-	}
-	String suit1 = card1.substring(3)
-	if(!(suit1 == "S" || suit1 == "H" || suit1 == "D" || suit1 == "C")){
-		validCard = false
-		println card1 + "is not a valid card"
-	}	
-} else {
-	String rank1 = card1.substring(0,1)
-	if(!(rank1 == "A" || rank1 == "2" || rank1 == "3" || rank1 == "4" || rank1 == "5" || rank1 == "6" || rank1 == "7" || rank1 == "8" || rank1 == "9" || rank1 == "J" || rank1 == "Q" || rank1 == "K")){
-		validCard = false
-		println card1 + "is not a valid card"
-	}
-	String suit1 = card1.substring(3)
-	if(!(suit1 == "S" || suit1 == "H" || suit1 == "D" || suit1 == "C")){
-		validCard = false
-		println card1 + "is not a valid card"
+boolean validRank(String r){
+	if(r == "A" || r == "2" || r == "3" || r == "4" || r == "5" || r == "6" || r == "7" || r == "8" || r == "9" || r == "10" || r == "J" || r == "Q" || r == "K"){
+		return true
+	} else {
+		return false
 	}
 }
 
-println "rank is: " + rank1
-println "suit is: " + suit1
-println "card is: " + card1
+boolean validSuit(String s){
+	if(s == "spades" || s == "hearts" || s == "diamonds" || s == "clubs"){
+		return true
+	} else {
+		return false
+	}
+}
+
+boolean validCard(String rk, String st){
+	if(validRank(rk) && validSuit(st)){
+		return true
+	} else {
+		return false
+	}
+}
+
+//Start of main program
+
+boolean finished = false
+
+for(i = 1; i <= 5; i++){
+	print "Please key in the rank of your card: "
+	String rank = System.console().readLine()
+	print "Please key in the suit of the card: "
+	String suit = System.console().readLine()	
+	if(validCard(rank,suit)){
+		if(i == 1){
+			String rank1 = rank
+			String suit1 = suit
+		} else if(i == 2){
+			String rank2 = rank
+			String suit2 = suit
+		} else if(i == 3){
+			String rank3 = rank
+			String suit3 = suit
+		} else if(i == 4){
+			String rank4 = rank
+			String suit4 = suit
+		} else if(i == 5){
+			String rank5 = rank
+			String suit5 = suit
+		}
+//		println "rank " + i + ": "  + rank			//test to check variables are being assigned correctly
+//		println "suit " + i + ": " + suit			//test to check variables are being assigned correctly
+	} else {
+		println "Card: " + rank + " of " + suit + " is NOT a valid card!"
+		finished = true
+		break
+	}
+}
+
